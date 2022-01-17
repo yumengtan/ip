90e3a822c8544a87af52c5bf888e35821d2839a0
yumengtan <yumengtan14@gmail.com>
2022-01-18 01:30:30 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,35 +2,62 @@
 //A0218371H
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
-    public static void main(String[] args) {
-        final String underscore = "____________________________________________________________";
-        //greetings
-        System.out.println(underscore);
 
-        System.out.println("(◍´◕ᴥ◕) Hello there! I'm Lucifer.\nWhat is it you desire today?");
-        System.out.println(underscore);
+    private static ArrayList<String> li = new ArrayList<>();
+    private static final String underscore = "____________________________________________________________";
+
+    public static void main(String[] args) {
+        //greetings
+        greeting();
 
         //get input
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
-            String repeat = sc.next();
-            System.out.println(underscore);
-            System.out.println(repeat);
-            System.out.println(underscore);
+            String repeat = sc.nextLine();
+            addList(repeat);
 
+            if (repeat.equals("list")) {
+                giveList();
+            }
             if (repeat.equals("bye")) {
                 //farewell message
-                System.out.println(underscore);
-                System.out.println("ʕ•́ᴥ•̀ʔっ Oh no! I hate to see you go:(");
-                System.out.println("Have I granted your desires?\nPlease come back again!");
-                System.out.println(underscore);
+                farewell();
                 break;
             }
         }
 
         sc.close();
 
+    }
+
+    
+    public static void addList(String adder) {
+        li.add(adder);
+        System.out.println(underscore);
+        System.out.println("added " + adder);
+        System.out.println(underscore);
+    }
+
+    public static void giveList() {
+        for (int i = 1; i < li.size(); i++) {
+            System.out.println(" " + i + ". " + li.get(i));
+        }
+        System.out.println(underscore);
+    }
+
+    public static void farewell() {
+        System.out.println(underscore);
+        System.out.println("ʕ•́ᴥ•̀ʔっ Oh no! I hate to see you go:(");
+        System.out.println("Have I granted your desires?\nPlease come back again!");
+        System.out.println(underscore);
+    }
+
+    public static void greeting() {
+        System.out.println(underscore);
+        System.out.println("(◍´◕ᴥ◕) Hello there! I'm Lucifer.\nWhat is it you desire today?");
+        System.out.println(underscore);
     }
 }
