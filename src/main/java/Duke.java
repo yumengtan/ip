1ece3196d0df190ce624edbbfa3e80f3b7bca6d7
yumengtan <yumengtan14@gmail.com>
2022-01-18 06:08:05 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class Duke {
 
-    private static ArrayList<String> li = new ArrayList<>();
+    private static ArrayList<Task> task = new ArrayList<>();
     private static final String underscore = "____________________________________________________________";
 
     public static void main(String[] args) {
@@ -16,16 +16,24 @@ public class Duke {
         //get input
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
-            String repeat = sc.nextLine();
-            addList(repeat);
+            String command = sc.nextLine();
 
-            if (repeat.equals("list")) {
+
+            if (command.equals("list")) {
                 giveList();
-            }
-            if (repeat.equals("bye")) {
+            } else if (command.equals("bye")) {
                 //farewell message
                 farewell();
                 break;
+            } else {
+                String[] commands = command.split(" ");
+                if (commands[0].equals("mark")) {
+                    mark(Integer.parseInt(commands[1]));
+                } else if (commands[0].equals("unmark")) {
+                    unmark(Integer.parseInt(commands[1]));
+                } else {
+                    addList(command);
+                }
             }
         }
 
@@ -33,17 +41,20 @@ public class Duke {
 
     }
 
-    
+
     public static void addList(String adder) {
-        li.add(adder);
+        Task curr = new Task(adder);
+        task.add(curr);
         System.out.println(underscore);
-        System.out.println("added " + adder);
+        System.out.println("added " + curr.description);
         System.out.println(underscore);
     }
 
     public static void giveList() {
-        for (int i = 1; i < li.size(); i++) {
-            System.out.println(" " + i + ". " + li.get(i));
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < task.size(); i++) {
+            int order = i + 1;
+            System.out.println(" " + order + ". " + task.get(i));
         }
         System.out.println(underscore);
     }
@@ -58,6 +69,22 @@ public class Duke {
     public static void greeting() {
         System.out.println(underscore);
         System.out.println("(◍´◕ᴥ◕) Hello there! I'm Lucifer.\nWhat is it you desire today?");
+        System.out.println(underscore);
+    }
+
+    public static void mark(int num) {
+        System.out.println(underscore);
+        System.out.println("Nice! I've marked this task as done: ");
+        task.get(num - 1).markDone();
+        System.out.println(task.get(num - 1).getStatusIcon());
+        System.out.println(underscore);
+    }
+
+    public static void unmark(int num) {
+        System.out.println(underscore);
+        System.out.println("OK, I've marked this task as not done yet: ");
+        task.get(num - 1).unmarkDone();
+        System.out.println(task.get(num - 1).getStatusIcon());
         System.out.println(underscore);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+//Author: Yu Meng
+//A0218371H
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void unmarkDone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return ((isDone ? "[X] " : "[ ] ") + this.description); // mark done task with X
+    }
+
+    @Override
+    public String toString() {
+        return getStatusIcon();
+    }
+
+}
