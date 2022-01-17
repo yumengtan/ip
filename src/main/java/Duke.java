fed31a51080b9a5b6e5b0f353468f0f312f36983
yumengtan <yumengtan14@gmail.com>
2022-01-18 01:03:28 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+//Author: Yu Meng
+//A0218371H
+
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        final String underscore = "____________________________________________________________";
+        //greetings
+        System.out.println(underscore);
+
+        System.out.println("(◍´◕ᴥ◕) Hello there! I'm Lucifer.\nWhat is it you desire today?");
+        System.out.println(underscore);
+
+        //get input
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String repeat = sc.next();
+            System.out.println(underscore);
+            System.out.println(repeat);
+            System.out.println(underscore);
+
+            if (repeat.equals("bye")) {
+                //farewell message
+                System.out.println(underscore);
+                System.out.println("ʕ•́ᴥ•̀ʔっ Oh no! I hate to see you go:(");
+                System.out.println("Have I granted your desires?\nPlease come back again!");
+                System.out.println(underscore);
+                break;
+            }
+        }
+
+        sc.close();
+
     }
 }
